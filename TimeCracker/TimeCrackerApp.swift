fbcb9b4848d93375b68d0e8f9c4a121f01421595
nnjh12 <nnjh12@gmail.com>
2023-05-31 14:53:50 -0400

    Initial Commit

---
+++ TimeCracker/TimeCrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCrackerApp.swift
+//  TimeCracker
+//
+//  Created by Gina on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeCrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
